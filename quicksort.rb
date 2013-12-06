55b8069fb82ce219d479ecfea17a3e632345a73c
YuchuanWeng <cts734@gmail.com>
2013-12-06 21:44:40 +0100

    'sort

--- quicksort.rb
+++ quicksort.rb
@@ -12,3 +12,39 @@ end
 
 puts (['can', 'feel', 'singing', 'like', 'a', 'can'].sort.join(' '))
 
+#Here is not a normal sort
+some_array = ['choco', 'banana', 'apple', 'kiki', 'water']
+def sort some_array
+  recursive_sort some_array, []
+end
+
+def recursive_sort unsorted, sorted
+  if unsorted.length <= 0
+    return sorted
+  end
+
+  smallest = unsorted.pop
+  still_unsorted = []
+
+  unsorted.each do |test_object|
+    if test_object < smallest
+      still_unsorted.push smallest
+      smallest = tested_object
+    else
+      still_unsorted.push tested_object
+    end
+  end
+  sorted.push smallest
+  recursive_sort still_unsorted, sorted
+end
+
+puts some_array.sort
+
+#Shuffle
+
+def shuffle arr
+  arr.sort_by(rand)
+end
+
+puts [1,2,3,4,5,6,7,8,89].shuffle
+
